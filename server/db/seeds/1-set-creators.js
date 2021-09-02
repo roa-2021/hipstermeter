916468dc8d2b61404507a34caca01b302e991e48
Matthew Searle <matthewjsearle@gmail.com>
2021-09-03 10:53:40 +1200

    update seeds

--- server/db/seeds/1-set-creators.js
+++ server/db/seeds/1-set-creators.js
@@ -1,13 +1,15 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('creators').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('creators').insert([
+        {id: 1, name: 'Matt', github_acc: 'mattsnz', my_fave: 'Black Sabbath', spotify_fave: ''},
+        {id: 2, name: 'Juliet', github_acc: 'JULIET-01', my_fave: 'Nils Frahm', spotify_fave: ''},
+        {id: 3, name: 'Shabu', github_acc: 'shabu-shrestha', my_fave: 'Selena Gomez', spotify_fave: ''},
+        {id: 3, name: 'Oscar', github_acc: 'oscarholt', my_fave: 'Radiohead', spotify_fave: ''},
+        {id: 3, name: 'Alex', github_acc: 'alexthomsonnz', my_fave: 'Hiatus Kaiyote', spotify_fave: 'J. Cole'}
       ]);
     });
 };

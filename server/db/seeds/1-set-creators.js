f81556b0b58b7ed65e03d1bc5483f2872ccd8849
Matthew Searle <matthewjsearle@gmail.com>
2021-09-03 10:59:37 +1200

    fix creator seeds

--- server/db/seeds/1-set-creators.js
+++ server/db/seeds/1-set-creators.js
@@ -8,8 +8,8 @@ exports.seed = function(knex) {
         {id: 1, name: 'Matt', github_acc: 'mattsnz', my_fave: 'Black Sabbath', spotify_fave: ''},
         {id: 2, name: 'Juliet', github_acc: 'JULIET-01', my_fave: 'Nils Frahm', spotify_fave: ''},
         {id: 3, name: 'Shabu', github_acc: 'shabu-shrestha', my_fave: 'Selena Gomez', spotify_fave: ''},
-        {id: 3, name: 'Oscar', github_acc: 'oscarholt', my_fave: 'Radiohead', spotify_fave: ''},
-        {id: 3, name: 'Alex', github_acc: 'alexthomsonnz', my_fave: 'Hiatus Kaiyote', spotify_fave: 'J. Cole'}
+        {id: 4, name: 'Oscar', github_acc: 'oscarholt', my_fave: 'Radiohead', spotify_fave: ''},
+        {id: 5, name: 'Alex', github_acc: 'alexthomsonnz', my_fave: 'Hiatus Kaiyote', spotify_fave: 'J. Cole'}
       ]);
     });
 };
